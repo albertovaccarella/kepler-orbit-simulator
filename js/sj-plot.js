992cda3a00aac6b214ae6904f82d0e1921752ea8
Alberto Vaccarella <vaccarella.alberto@gmail.com>
2016-10-17 12:25:11 +0200

    Add plotting (3D and x, y, z time series)

--- js/sj-plot.js
+++ js/sj-plot.js
@@ -3,7 +3,7 @@ var configOptions = {
     displaylogo: false
 };
 
-function plotProjection(plotDiv, xAxisData, yAxisData) {
+function plotProjection(plotDiv, xName, yName, xAxisData, yAxisData) {
 
     var trace = {
         type: 'scatter',                    // set the chart type
@@ -18,7 +18,7 @@ function plotProjection(plotDiv, xAxisData, yAxisData) {
     };
 
     var layout = {
-            title: xName + ' - ' + yName,
+            title:  xName + ' - ' + yName,
             margin: {
                 l: 15,
                 r: 15,
@@ -30,4 +30,46 @@ function plotProjection(plotDiv, xAxisData, yAxisData) {
         };
 
         Plotly.newPlot(plotDiv, [trace], layout, configOptions);
+}
+
+function plot3D(plotDiv, plotName, x, y, z) {
+    var trace = {
+        type: 'scatter3d',                    // set the chart type
+        mode: 'lines',                      // connect points with lines
+        x: x,
+        y: y,
+        z: z,
+        opacity: 1,
+        line: {                             // set the width of the line.
+            width: 1,
+            reversescale: false
+        }
+    };
+
+    var layout = {
+            title: plotName,
+            margin: {
+                l: 15,
+                r: 15,
+                b: 50,
+                t: 100,
+                pad: 4
+              },
+            scene:{
+                xaxis: {
+                    showbackground: true,
+                    backgroundcolor: "rgb(230, 230,200)"
+                },
+                yaxis: {
+                    showbackground: true,
+                    backgroundcolor: "rgb(230, 230,200)"
+                },
+                zaxis: {
+                    showbackground: true,
+                    backgroundcolor: "rgb(230, 230,200)"
+                }
+            }
+        };
+
+    Plotly.newPlot(plotDiv, [trace], layout, configOptions);
 }
